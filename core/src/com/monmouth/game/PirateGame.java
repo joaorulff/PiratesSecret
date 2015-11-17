16d0e8f15ab265a22e8ce11d5130f2b184793aa6
Matheus Bernardo <matheussbernardo@gmail.com>
2015-11-17 00:31:55 -0500

    Creating second screen

--- core/src/com/monmouth/game/PirateGame.java
+++ core/src/com/monmouth/game/PirateGame.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.monmouth.screens.PlayScreen;
+import com.monmouth.screens.StartScreen;
 
 /**
  * Created by joaolucasrulffdacosta on 10/28/15.
@@ -16,7 +18,7 @@ public class PirateGame extends Game {
 
 
     public SpriteBatch batch;
-
+    public BitmapFont font;
     public static final int V_WIDTH = 400;
     public static final int V_HEIGHT = 208;
     public static final float PPM = 100;
@@ -30,11 +32,12 @@ public class PirateGame extends Game {
     public void create (){
 
         batch = new SpriteBatch();
+        font = new BitmapFont();
         assetManager = new AssetManager();
         assetManager.load("audio/music/pirateMusic.mp3", Music.class);
         assetManager.load("audio/sounds/pirateJump.wav", Sound.class);
         assetManager.finishLoading();
-        setScreen(new PlayScreen(this));
+        this.setScreen(new StartScreen(this));
 
     }
 
